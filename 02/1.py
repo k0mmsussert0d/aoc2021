c0daa15c26d339f4f6f275875aecf47598c140d6
Maksymilian Babarowski <maksymilian@babarowski.pl>
2021-12-02 10:28:57 +0100

    Day 2

---
+++ 02/1.py
@@ -0,0 +1,17 @@
+from functools import reduce
+
+
+if __name__ == '__main__':
+    def func(res, line):
+        match line.split(' '):
+            case ['forward', value]:
+                res[0] += int(value)
+            case ['up', value]:
+                res[1] -= int(value)
+            case ['down', value]:
+                res[1] += int(value)
+        return res
+
+    with open('input', 'r') as f:
+        cords = reduce(func, f.readlines(), [0, 0])
+        print(cords[0] * cords[1])
