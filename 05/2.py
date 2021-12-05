cbdb8a7d56d30a6aa41e2b4a975451decd900150
Maksymilian Babarowski <maksymilian.babarowski@gmail.com>
2021-12-05 16:41:49 +0100

    Day 5

---
+++ 05/2.py
@@ -0,0 +1,34 @@
+import re
+
+
+if __name__ == '__main__':
+    with open('input', 'r') as file:
+        vents = [tuple([int(i) for i in re.compile(r'(\d+),(\d+) -> (\d+),(\d+)').match(line).groups()]) for line in file.readlines()]
+    matrix = []
+    for _ in range(1000):
+        matrix.append([0] * 1000)
+    for ax, ay, bx, by in vents:
+        if ay == by:
+            if ax < bx:
+                start = ax
+                end = bx
+            else:
+                start = bx
+                end = ax
+            for zx in range(start, end + 1):
+                matrix[by][zx] += 1
+        elif ax == bx:
+            if ay < by:
+                start = ay
+                end = by
+            else:
+                start = by
+                end = ay
+            for zy in range(start, end + 1):
+                matrix[zy][bx] += 1
+        else:
+            s = 1 if bx > ax else -1
+            t = 1 if by > ay else -1
+            for zx, zy in zip(range(ax, bx + s, s), range(ay, by + t, t)):
+                matrix[zy][zx] += 1
+    print(sum([len(list(filter(lambda x: x > 1, i))) for i in matrix]))
